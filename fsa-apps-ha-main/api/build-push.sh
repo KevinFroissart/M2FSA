d26bb78dfd9fbf7e92a358288daf90a6d7d32625
KevinFroissart <kevin.froissart69@gmail.com>
2023-04-19 12:29:37 +0200

    harbor job & update fsa upload tags

--- fsa-apps-ha-main/api/build-push.sh
+++ fsa-apps-ha-main/api/build-push.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd $(dirname $0)
-test -z "$TAG" && TAG=harbor.fsa.os.univ-lyon1.fr/fsa/api:latest
+test -z "$TAG" && TAG=harbor.fsa.os.univ-lyon1.fr/prj-15/api:latest
 MIRROR_SWITCH=""
 test -n "$MVN_MIRROR" && MIRROR_SWITCH="--build-arg MVN_MIRROR"
 docker build $MIRROR_SWITCH -t "$TAG" . && docker push "$TAG"
